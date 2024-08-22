a549b5545b279482c93b4835a3b4264b79d3bece
HenriqueChaves <henriquelopes0611@gmail.com>
2024-08-21 22:34:29 -0300

    init `class03`

---
+++ class03/numbases.cpp
@@ -0,0 +1,15 @@
+// C++ can represent numbers in several bases.
+// cout converts automatically for decimal base
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "Testing number bases in C++.\n";
+    cout << 25 << endl;
+    cout << 0x19 << endl;
+    cout << 031 << endl;
+    cout << 0b11001 << endl;
+
+    return 0;
+}
